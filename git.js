38abd954a4f51ffe19f4584d95a0b2721177d1e9
Vincent <ihaveagoodlife123456789@gmail.com>
2026-06-22 16:19:37 -0400

    Added more interatibility

--- git.js
+++ git.js
@@ -1,10 +1,30 @@
-let word = document.querySelector('h1')
+let wordOne = document.querySelector('h1')
+let wordTwo = document.querySelector('h2')
+let wordFour = document.querySelector('h4')
+
+let added = 0
+
 let randomNum = () => {
  return Math.floor(Math.random() * 256)
 }
 
-const randomColor = () => {
-     word.style.color = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`
+const increment = () => {
+     return incremented(added++)
 }
 
-word.addEventListener('click', randomColor)
+const incremented = (func) => {
+     if (func > 100) {
+          wordFour.innerHTML = '100/100'
+          wordTwo.style.display = 'block'
+     } else {
+     wordFour.innerHTML = `${func}/100`
+     }
+}
+
+
+const randomColor = () => {
+     wordOne.style.color = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`
+}
+
+wordOne.addEventListener('click', randomColor)
+wordOne.addEventListener('click', increment)
